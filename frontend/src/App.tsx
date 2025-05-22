cf6885c31aaa49568e0b82b2a11e2b82d2331d74
John Doe <bhattsonali36@gmail.com>
2025-05-22 16:00:46 +0530

    error handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { Route, Routes } from 'react-router-dom'
 import RoomJoinPage from './RoomJoinPage'
 import ChatRoom from './ChatRoom'
--- frontend/src/ChatRoom.tsx
+++ frontend/src/ChatRoom.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react"
 import { useParams } from "react-router-dom";
-import { motion, AnimatePresence } from "motion/react"
+import { motion } from "motion/react"
 import Rooms from "./Rooms";
 
 
@@ -34,7 +34,7 @@ function ChatRoom() {
     inputRef.current.value = " ";
     
   }
-  const handleKeyPress=(event)=>{
+  const handleKeyPress=(event: { key: string; shiftKey: any; })=>{
     if (event.key === 'Enter' && !event.shiftKey) {
        
         sendMessage(); // Trigger send message
